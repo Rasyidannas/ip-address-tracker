55c477fe3f9e0a1e2611d938cea2aece5df7f9e8
Rasyid <61853270+Rasyidannas@users.noreply.github.com>
2023-12-23 21:48:16 +0000

    feat: map with leaflet added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import Header from "./components/Layouts/Header";
 import ResultData from "./components/Result/ResultData";
 import DataProvider from "./store/DataProvider.jsx";
+import Map from "./components/Layouts/Map";
 
 function App() {
   return (
     <DataProvider>
       <Header />
       <ResultData className="relative -top-12 left-1/2 -translate-x-1/2 rounded-lg shadow-sm bg-white" />
+      <div className="leaflet-container">
+        <Map />
+      </div>
     </DataProvider>
   );
 }

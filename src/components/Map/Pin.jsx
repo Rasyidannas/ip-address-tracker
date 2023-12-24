83659c66e2c4ac3bfc6e254ede0f7b426d3acefb
Rasyid <61853270+Rasyidannas@users.noreply.github.com>
2023-12-24 14:01:55 +0000

    fix: pin.jsx

--- src/components/Map/Pin.jsx
+++ src/components/Map/Pin.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
-import { Marker, Popup, useMap, SVGOverlay } from "react-leaflet";
-import IconPin from "../Icons/IconPin";
+import { Marker, Popup, useMap } from "react-leaflet";
 
 const Pin = function ({ onLat, onLng, onLocation }) {
   const map = useMap();
@@ -9,8 +8,6 @@ const Pin = function ({ onLat, onLng, onLocation }) {
   }, [onLat, onLng]);
 
   return (
-    // <IconPin className="relative top-1/2 left-1/2 z-[1000] -translate-y-1/2 -translate-x-1/2 w-12" />
-
     <Marker position={[onLat, onLng]}>
       <Popup>
         <p className="text-center">
